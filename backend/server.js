a04ee13b11839a9846ecc445a8d3c30c327dbe5b
Alleen Uzcategui <alleenu887@gmail.com>
2023-08-23 23:29:44 -0300

    verificacion de rutas

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors"; // Importa el módulo 'cors'
+import cors from "cors"; 
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
@@ -22,16 +22,12 @@ mongoose
 
 const app = express();
 
-app.use(cors()); // Agrega el middleware 'cors'
-
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-});
-app.get("/api/keys/google", (req, res) => {
-  res.send({ key: process.env.GOOGLE_API_KEY || "" });
 });
 
 app.use("/api/upload", uploadRouter);
@@ -40,17 +36,21 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
+// Serve static files from the 'build' folder of the frontend
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+// Send the 'index.html' file for all routes (frontend will handle routing)
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
